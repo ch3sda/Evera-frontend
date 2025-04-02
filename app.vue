88caafad58b4c35dc3bc4c461f083a54d789a96b
ch3sda <officialchesdamc@gmail.com>
2025-04-02 15:44:45 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+      <p class="text-green-500 text-2xl font-bold">
+        Hello world!
+      </p>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/assets/css/tailwind.css'],
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxt/ui',
+    '@formkit/auto-animate'
+  ]
+})

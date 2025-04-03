c6811d4345106b13c4dd0c4005d36d707a9bc347
ch3sda <officialchesdamc@gmail.com>
2025-04-03 21:21:18 +0700

    check update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,20 @@
+<template>
+    <div class="h-screen bg-white dark:bg-gray-900">
+        <div class=" bg-white dark:bg-gray-900">
+            <slot />
+        </div>
+        <CommonBottomNavigation />
+    </div>
+</template>
+
 <script setup>
     import { onMounted } from 'vue';
+
     // Initialize Flowbite components when the page is mounted
     onMounted(() => {
     if (typeof window.initFlowbite === 'function') {
         window.initFlowbite();
     }
-});
+    });
 </script>
 
-<template>
-    <div>
-        <h1 class="text-yellow-500"> Hello World </h1>
-    </div>
-</template> 

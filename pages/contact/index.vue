1f299fa2f5e14bd9c061197535176f63732e3f21
ch3sda <officialchesdamc@gmail.com>
2025-06-09 20:09:34 +0700

    Landing Finish

---
+++ pages/contact/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-screen flex items-center justify-center">
+        <CommonContactForm />
+    </div>
+</template>
+
+
+<script setup>
+
+    definePageMeta({
+        layout: 'landing',
+    }) 
+</script>
---
+++ pages/service/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-screen flex items-center justify-center">
+        <CommonService />
+    </div>
+</template>
+
+
+<script setup>
+
+    definePageMeta({
+        layout: 'landing',
+    }) 
+</script>
